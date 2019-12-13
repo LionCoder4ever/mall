683a9a42a2f79fdb04e05a75c56fc924f94230c2
kzwcoder <kzwcoder2015@163.com>
2019-12-14 00:19:29 +0800

    upload grpc request code

--- app/service/main/account/cmd/main.go
+++ app/service/main/account/cmd/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	gogrpc "google.golang.org/grpc"
+	"mall/app/service/main/account/api"
 	"mall/app/service/main/account/conf"
+	"mall/app/service/main/account/server/grpc"
 	"mall/app/service/main/account/server/http"
 	"mall/app/service/main/account/service"
 	"mall/library/log"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -25,7 +29,23 @@ func main() {
 	// init service
 	svc := service.New(&conf.Conf)
 	// init http server
-	http.New(svc)
+	go http.New(svc)
+
+	log.Info("prepare grpc server init success")
+	go func() {
+		lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9013))
+		if err != nil {
+			log.Infof("failed to listen: %v", err)
+		}
+		log.Infof("grpc server listen on %s", lis.Addr())
+		grpcServer := gogrpc.NewServer()
+		api.RegisterAccountServer(grpcServer, grpc.New(svc))
+		err = grpcServer.Serve(lis)
+		if err != nil {
+			log.Infof("failed to listen: %v", err)
+		}
+		log.Info("grpc server init success")
+	}()
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
 	for {
--- go.mod
+++ go.mod
@@ -7,8 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-sql-driver/mysql v1.4.1
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/golang/protobuf v1.3.2
+	github.com/gogo/protobuf v1.3.1
 	github.com/jinzhu/gorm v1.9.11
 	github.com/json-iterator/go v1.1.8 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
