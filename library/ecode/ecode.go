d9ab0b9aa9163aa8ce2de8c6e174970335a33e2f
kzwcoder <kzwcoder2015@163.com>
2020-01-06 23:16:15 +0800

    add business ecode struct

---
+++ library/ecode/ecode.go
@@ -0,0 +1,48 @@
+package ecode
+
+var (
+	_messages atomic.Value
+	_codes    = map[int]struct{}{}
+)
+
+func Register(cm map[int]string) {
+	_messages.Store(cm)
+}
+
+func New(e int) Code {
+	if e <= 0 {
+		panic("business ecode must greater than zero")
+	}
+	return add(e)
+}
+
+func add(e int) Code {
+	if _, ok := _codes[e]; ok {
+		panic(fmt.Sprintf("ecode: %d already exist", e))
+	}
+	_codes[e] = struct{}{}
+	return Int(e)
+}
+
+type Codes interface {
+	Error() string
+	Code() int
+	Message() string
+}
+
+type Code int
+
+func (e Code) Error() string {
+	return strconv.FormatInt(int64(e), 10)
+}
+
+func (e Code) Code() int { return int(e) }
+
+func (e Code) Message() string {
+	if cm, ok := _messages.Load().(map[int]string); ok {
+		if msg, ok := cm[e.Code()]; ok {
+			return msg
+		}
+	}
+	return e.Error()
+}
